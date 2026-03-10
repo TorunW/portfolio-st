0fa9c3b893f0bf0f8b7cd74df905bb716c417bcf
TorunW <torun.wikstrom@gmail.com>
2026-03-10 12:37:53 +0100

    build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' nighteye='disabled'>
+    <html lang='en' data-nighteye='disabled'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
